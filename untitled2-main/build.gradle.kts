cda31df4936a076a7306a093cce13d1aaa494ab8
xenimini <kseniya.feo2000@gmail.com>
2026-04-23 13:37:50 +0300

    Update build.gradle.kts

--- untitled2-main/build.gradle.kts
+++ untitled2-main/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "2.2.21"
+    kotlin("plugin.serialization") version "2.2.21"
 }
 
 group = "org.example"
@@ -11,6 +12,8 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.0")
     implementation("de.fabmax.kool:kool-core:0.19.0")
     implementation("de.fabmax.kool:kool-physics:0.19.0")
 }
